a8ac86185f8d337acb3b8ee44a86909e7828879d
xiaohe <xiaohe@hiynn.com>
2018-04-11 13:53:27 +0800

    整理讲解的提交

--- book.config.js
+++ book.config.js
@@ -2,13 +2,13 @@
  * Created by xiaohe on 2018/2/24.
  */
 const book = {
-    'url': 'http://www.uctxt.com/book/2/2054/',//这是小说的目录页
-    'href': 'http://www.uctxt.com/book/2/2054/',//这是小说的目录页
+    'url': 'http://www.uctxt.com/book/5/5632/',//这是小说的目录页
+    'href': 'http://www.uctxt.com/book/5/5632/',//这是小说的目录页
     'charset':'gb2312',//字符集支持 UTF-8 或 gb2312
-    'name': '天启之门',//设定保存的书名
+    'name': '修真聊天群',//设定保存的书名
     'listDom': '.chapter-list dd a',//小说列表的内容html结构
     'artTitle': '#BookTitle',//每一篇的标题html结构
     'artContent': '#content',//每一篇的内容html结构
-    'index': 0//从某章节索引开始
+    'index': 736//从某章节索引开始
 };
 module.exports = book;
